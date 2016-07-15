1b627c1aed5fab3d186436c254116ddcba1444bc
Shawn Fenerin <sfenerin@stanford.edu>
2016-07-14 20:20:57 -0700

    idk what i did

--- app/src/main/java/com/example/sfene_000/project_ecourage/breathingActivity.java
+++ app/src/main/java/com/example/sfene_000/project_ecourage/breathingActivity.java
@@ -1,13 +1,24 @@
 package com.example.sfene_000.project_ecourage;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class breathingActivity extends AppCompatActivity {
-
+    boolean popUpsPresent =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathing);
+
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        if( sharedPref.getBoolean("openedBreathing",false) ){
+            popUpsPresent=true;
+        }
+
+        //SharedPreferences.Editor editor = sharedPref.edit();
+
     }
 }
