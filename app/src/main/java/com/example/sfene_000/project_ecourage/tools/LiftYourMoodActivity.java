c2b170d3192b17c5874f0201e056474b98f31224
sfenerin <sfenerin@stanford.edu>
2016-07-20 14:32:27 -0700

    moved buttons and moved activities into packages

--- app/src/main/java/com/example/sfene_000/project_ecourage/tools/LiftYourMoodActivity.java
+++ app/src/main/java/com/example/sfene_000/project_ecourage/tools/LiftYourMoodActivity.java
@@ -1,7 +1,9 @@
-package com.example.sfene_000.project_ecourage;
+package com.example.sfene_000.project_ecourage.tools;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.sfene_000.project_ecourage.R;
 
 public class LiftYourMoodActivity extends AppCompatActivity {
 
