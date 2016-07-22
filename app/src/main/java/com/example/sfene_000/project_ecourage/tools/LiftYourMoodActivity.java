5d34a96161a467a5d8f11f03b2e603d87692835e
Shawn Fenerin <sfenerin@stanford.edu>
2016-07-21 20:42:40 -0700

    changed viewFlipper to viewpager

--- app/src/main/java/com/example/sfene_000/project_ecourage/tools/LiftYourMoodActivity.java
+++ app/src/main/java/com/example/sfene_000/project_ecourage/tools/LiftYourMoodActivity.java
@@ -1,5 +1,11 @@
 package com.example.sfene_000.project_ecourage.tools;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ViewFlipper;
@@ -7,36 +13,57 @@ import android.view.MotionEvent;
 import android.app.Activity;
 
 import com.example.sfene_000.project_ecourage.R;
+import com.example.sfene_000.project_ecourage.tools.LiftYourMoodFragments.tools_lym_p1;
 
-public class LiftYourMoodActivity extends AppCompatActivity {
-    ViewFlipper VF;
-    float X1;
+
+public class LiftYourMoodActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 8;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lift_your_mood);
-        VF = (ViewFlipper) findViewById(R.id.viewFlipper);
-        VF.setInAnimation(this,android.R.anim.slide_out_right);
-        VF.setOutAnimation(this,android.R.anim.slide_in_left);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
     }
-    public boolean onTouchEvent(MotionEvent touchevent) {
-        switch (touchevent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                X1 = touchevent.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                float finalX = touchevent.getX();
-                if (X1 > finalX) {
-                    if (VF.getDisplayedChild() == 1)
-                        break;
-                    VF.showNext();
-                } else {
-                    if (VF.getDisplayedChild() == 0)
-                        break;
-                    VF.showPrevious();
-                }
-                break;
+
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
         }
-        return false;
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new tools_lym_p1();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
+
