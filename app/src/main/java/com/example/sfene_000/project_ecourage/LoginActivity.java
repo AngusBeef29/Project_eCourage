e4b1fdc114c69a977710bd8014594e2c0bda62b0
sfenerin <sfenerin@stanford.edu>
2016-07-29 16:54:56 -0700

    working log in and sign upgit add .git add . Connected to the backendgit add .! needs style fixes though

--- app/src/main/java/com/example/sfene_000/project_ecourage/LoginActivity.java
+++ app/src/main/java/com/example/sfene_000/project_ecourage/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.example.sfene_000.project_ecourage;
 
-import android.app.Activity;
+import  android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +19,18 @@ import com.example.sfene_000.project_ecourage.R;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import org.json.*;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,17 +52,22 @@ public class LoginActivity extends AppCompatActivity {
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
 
-        String passwordHash = encode(password);
-        Log.d("LOGINTEST:onClick", "encode(" + password + "): " + passwordHash);
 
-        if (loginSuccessful(username, password)) {
-            Log.d("LOGINTEST:onClick", "success!");
-            Intent intent = new Intent(view.getContext(), MainActivity.class);
-            setResult(Activity.RESULT_OK, intent);
-            startActivity(intent);
-        } else {
-            Log.d("LOGINTEST:onClick", "fail.");
-        }
+        String[] params = new String[]{username, password};
+        LogInUser signUpUser = new LogInUser(this);
+        signUpUser.execute(params);
+        //String passwordHash = encode(password);
+
+       // Log.d("LOGINTEST:onClick", "encode(" + password + "): " + passwordHash);
+
+       // if (loginSuccessful(username, password)) {
+//            Log.d("LOGINTEST:onClick", "success!");
+//            Intent intent = new Intent(view.getContext(), MainActivity.class);
+//            setResult(Activity.RESULT_OK, intent);
+//            startActivity(intent);
+//        } else {
+//            Log.d("LOGINTEST:onClick", "fail.");
+//        }
     }
 
     public void signUp(View view) {
@@ -58,25 +76,6 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private boolean loginSuccessful(String username, String password) {
-        if (userExists(username.toString()) && passwordMatch(password.toString())) {
-            Log.d("LOGINTEST:logic", "username and password match!");
-            return true;
-        } else {
-            Log.d("LOGINTEST:logic", "username/password do not match.");
-            return false;
-        }
-    }
-
-    //STUB IMPLEMENTATION
-    private boolean userExists(String usernameString) {
-        return false;
-    }
-
-    //STUB IMPLEMENTATION
-    private boolean passwordMatch(String passwordHash) {
-        return false;
-    }
 
     private static String encode(String password) {
         MessageDigest md;
@@ -103,4 +102,71 @@ public class LoginActivity extends AppCompatActivity {
         }
         return buff.toString();
     }
+
+    private class LogInUser extends AsyncTask<String, Void, String> {
+        private Activity activity;
+        public LogInUser(Activity activity){
+            this.activity= activity;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+            Random randomGenerator = new Random();
+            if(username.length()>0 && password.length()>0){
+                String url = "http://ecourage.org/sql_query.php?nFunction=2&username="+username+"&password="+password;
+                URL obj = null;
+                HttpURLConnection con = null;
+                try {
+                    obj = new URL(url);
+                    con = (HttpURLConnection) obj.openConnection();
+                    con.setRequestMethod("GET");
+                    con.connect();
+                    int responseCode = con.getResponseCode();
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    JSONObject json = new JSONObject(response.toString());
+                    Log.d("BUTT",json.get("message").toString());
+                    String responseMessage = json.get("message").toString();
+                    if(responseMessage.equals("log in successful")){
+                        return "logged in";
+                    } else{
+                        return "wrong password";
+                    }
+                } catch (MalformedURLException e) {
+                    return "error";
+                } catch (IOException e) {
+                    return "error";
+                } catch (JSONException e) {
+                    return "error";
+                } finally {
+                    con.disconnect();
+                }
+
+            }
+            return "empty field";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(result.equals("empty field")){
+                TextView error =(TextView) findViewById(R.id.error);
+                error.setText("One or more fields is missing your information!");
+            } else if(result.equals("wrong password")) {
+                TextView error =(TextView) findViewById(R.id.error);
+                error.setText("incorrect passcode");
+            } else {
+                Intent intent = new Intent(activity, MainActivity.class);
+                setResult(Activity.RESULT_OK, intent);
+                startActivity(intent);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/sfene_000/project_ecourage/SignUpActivity.java
+++ app/src/main/java/com/example/sfene_000/project_ecourage/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.sfene_000.project_ecourage;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,50 +13,162 @@ import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText newPasswordField;
     EditText confirmPasswordField;
-
+    EditText emailField;
+    EditText usernameField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
         newPasswordField = (EditText) findViewById(R.id.newPasswordField);
         newPasswordField.setTypeface(Typeface.DEFAULT);
         newPasswordField.setTransformationMethod(new PasswordTransformationMethod());
+
         confirmPasswordField = (EditText) findViewById(R.id.confirmPasswordField);
         confirmPasswordField.setTypeface(Typeface.DEFAULT);
         confirmPasswordField.setTransformationMethod(new PasswordTransformationMethod());
+
+        emailField = (EditText) findViewById(R.id.emailField);
+
+       usernameField = (EditText) findViewById(R.id.usernameField);
 
     }
 
     public void signUp(View view) {
 
+
         String newPassword = newPasswordField.getText().toString();
         String confirmPassword = confirmPasswordField.getText().toString();
 
-        String username = "STUB";
+        String username = usernameField.getText().toString();;
+        String email = emailField.getText().toString();
+
 
         if (!newPassword.equals(confirmPassword)) {
-            Log.d("SIGNUP:onClick", "password fields do not match!");
-            return;
-        }
-        if (userExists(username)) {
-            Log.d("SIGNUP:onClick", "username already taken!");
+            TextView error =(TextView) findViewById(R.id.error);
+            error.setText("Passwords don't match");
             return;
         }
 
-        // newUser(email, username, password, isCoach);
+        String[] params = new String[]{username, newPassword,email};
+        SignUpUser signUpUser = new SignUpUser(this);
+        signUpUser.execute(params);
 
-        Intent intent = new Intent(view.getContext(), MainActivity.class);
-        setResult(Activity.RESULT_OK, intent);
-        startActivity(intent);
     }
 
-    // STUB IMPLEMENTATION
-    private boolean userExists(String username) {
-        return false;
+    private static String encode(String password) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            byte[] passwordBytes = password.getBytes();
+            byte[] hashBytes = md.digest(passwordBytes);
+            String hash = hexToString(hashBytes);
+            return hash;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    private static String hexToString(byte[] bytes) {
+        StringBuffer buff = new StringBuffer();
+        for (int i = 0; i < bytes.length; i++) {
+            int val = bytes[i];
+            val = val & 0xff; // remove higher bits, sign
+            if (val < 16)
+                buff.append('0'); // leading 0
+            buff.append(Integer.toString(val, 16));
+        }
+        return buff.toString();
+    }
+
+    private class SignUpUser extends AsyncTask<String, Void, String> {
+        private Activity activity;
+        public SignUpUser(Activity activity){
+            this.activity= activity;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            Random randomGenerator = new Random();
+            if(username.length()>0 && password.length()>0 && email.length()>0){
+                String url = "http://ecourage.org/sql_query.php?nFunction=4&username="+username+"&password="+password+"&coach_code="+ randomGenerator.nextInt(100000) +"&email="+email;
+                URL obj = null;
+                HttpURLConnection con = null;
+                try {
+                    obj = new URL(url);
+                    con = (HttpURLConnection) obj.openConnection();
+                    con.setRequestMethod("GET");
+                    con.connect();
+                    int responseCode = con.getResponseCode();
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    JSONObject json = new JSONObject(response.toString());
+                    Log.d("BUTT",json.get("message").toString());
+                    String responseMessage = json.get("message").toString();
+                    if(responseMessage.equals("user successfully created")){
+                        return "user created";
+                    } else{
+                        return "user exists already";
+                    }
+                } catch (MalformedURLException e) {
+                    return "error";
+                } catch (IOException e) {
+                    return "error";
+                } catch (JSONException e) {
+                    return "error";
+                } finally {
+                    con.disconnect();
+                }
+
+            }
+            return "empty field";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(result.equals("empty field")){
+                TextView error =(TextView) findViewById(R.id.error);
+                error.setText("One or more fields is missing your information!");
+            } else if(result.equals("user exists already")) {
+                TextView error =(TextView) findViewById(R.id.error);
+                error.setText("that username is already taken. Try another!");
+            } else if(result.equals("error")) {
+                TextView error =(TextView) findViewById(R.id.error);
+                error.setText("Something went wrong on our end :( Try again Later");
+            } else {
+                Intent intent = new Intent(activity, MainActivity.class);
+                setResult(Activity.RESULT_OK, intent);
+                startActivity(intent);
+            }
+        }
+    }
+
 }
